95d677bcb90a7ec9aac1064d5561bebcbae5f568
Nitesh Basnet <naam.nitesh@gmail.com>
2016-03-12 17:37:10 -0600

    Project setup

---
+++ src/main/java/com/hashmount/IssueloggerApplication.java
@@ -0,0 +1,12 @@
+package com.hashmount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueloggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssueloggerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hashmount/IssueloggerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hashmount;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IssueloggerApplication.class)
+public class IssueloggerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
